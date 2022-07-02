e8bae0b918f3b8ef67f6b40a323487b5b80f8619
elther <elther@naver.com>
2022-07-02 23:04:06 +0900

    Initial commit

---
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,29 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     *
+     * @param loginId
+     * @param password
+     * @return null 이면 실패
+     */
+    public Member login(String loginId, String password){
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
